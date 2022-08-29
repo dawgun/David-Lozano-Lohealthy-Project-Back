7213fbd43972318bcb4b6bbba8dd5d1a19a721d0
David Lozano <dawgun@hotmail.com>
2022-08-29 18:58:36 +0200

    Added connectDB to index for launch server mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "./loadEnvironment";
+import connectDB from "./database";
 import startServer from "./server/startServer";
 
 const port = +process.env.PORT || 4000;
+const mongoURL = process.env.MONGODB_URL;
 
 (async () => {
   try {
     await startServer(port);
+    await connectDB(mongoURL);
   } catch (error) {
     process.exit(1);
   }

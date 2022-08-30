a8267f4d2ceb5f89ec3da29bedcb92a4c1f4482d
David Lozano <dawgun@hotmail.com>
2022-08-30 16:43:15 +0200

    Added test for if with error express-validation generalError middleware

---
+++ src/utils/CustomTestExpressError/CustomExpressError.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from "express-validation";
+
+class CustomTestErrorExpress extends ValidationError {
+  code: string;
+
+  constructor(
+    public statusCode: number,
+    public privateMessage: string,
+    public publicMessage: string
+  ) {
+    super({ body: [] }, {});
+  }
+}
+
+export default CustomTestErrorExpress;

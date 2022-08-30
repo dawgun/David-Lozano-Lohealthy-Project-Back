5876f5abf9b8ee320854788652817b86e25c350f
David Lozano <dawgun@hotmail.com>
2022-08-30 09:50:16 +0200

    Created general error middleware

--- src/server/middlewares/error/errors.ts
+++ src/server/middlewares/error/errors.ts
@@ -1,14 +1,28 @@
 import "../../../loadEnvironment";
 import Debug from "debug";
 import chalk from "chalk";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import CustomError from "../../../utils/CustomError/CustomError";
 
 const debug = Debug("lohealthy:server:middlewares/errors");
 
-const endpointError = (req: Request, res: Response) => {
+export const endpointError = (req: Request, res: Response) => {
   res.status(404).json({ error: "Endpoint not found" });
 
   debug(chalk.red("Client sends an unknown endpoint"));
 };
 
-export default endpointError;
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
+  const errorCode = error.statusCode ?? 500;
+  const errorMessage = error.publicMessage ?? "Everything has peted";
+
+  debug(chalk.red(error.message));
+
+  res.status(errorCode).json({ error: errorMessage });
+};

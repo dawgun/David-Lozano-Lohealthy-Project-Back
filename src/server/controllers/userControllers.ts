ff79b494a3baa872e50717e8db19e3c798a9f452
David Lozano <dawgun@hotmail.com>
2022-08-30 10:59:33 +0200

    Created userRegister controller

---
+++ src/server/controllers/userControllers.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../../database/models/User";
+import LohealthyUser from "../../types/user";
+import hashCreator from "../../utils/auth/auth";
+import CustomError from "../../utils/CustomError/CustomError";
+
+const userRegister = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const userData: LohealthyUser = req.body;
+  userData.password = await hashCreator(userData.password);
+
+  try {
+    const newUser = await User.create(userData);
+
+    res.status(201).json({ user: newUser });
+  } catch (error) {
+    const customError = new CustomError(
+      400,
+      error.message,
+      "Error creating new user"
+    );
+    next(customError);
+  }
+};
+
+export default userRegister;

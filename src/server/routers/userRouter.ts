61e33999fe3e6b939d630d1af6e621bbd3e44e1b
David Lozano <dawgun@hotmail.com>
2022-08-30 13:42:49 +0200

    Added validate with registerValidation in userRouter

--- src/server/routers/userRouter.ts
+++ src/server/routers/userRouter.ts
@@ -1,8 +1,14 @@
 import express from "express";
+import { validate } from "express-validation";
+import registerValidation from "../../schemas/userCredentialsSchema";
 import userRegister from "../controllers/userControllers";
 
 const userRouter = express.Router();
 
-userRouter.post("/register", userRegister);
+userRouter.post(
+  "/register",
+  validate(registerValidation, {}, { abortEarly: false }),
+  userRegister
+);
 
 export default userRouter;

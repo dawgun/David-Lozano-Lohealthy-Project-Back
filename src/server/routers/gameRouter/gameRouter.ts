339dba94968a736eb168b5cb76645ba3af82889d
David Lozano <dawgun@hotmail.com>
2022-09-07 12:53:37 +0200

    Added in gameRoute, createGame with multer and userAuthorization

--- src/server/routers/gameRouter/gameRouter.ts
+++ src/server/routers/gameRouter/gameRouter.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import multer from "multer";
 import {
+  createGame,
   deleteGame,
   getAllGames,
 } from "../../controllers/gameControllers/gameControllers";
+import userAuthentification from "../../middlewares/userAuthentification/userAuthentification";
 
 const gameRouter = express.Router();
+const upload = multer({ dest: "uploads/" });
 
 gameRouter.get("/", getAllGames);
+gameRouter.post(
+  "/create",
+  upload.single("image"),
+  userAuthentification,
+  createGame
+);
 gameRouter.delete("/delete/:idGame", deleteGame);
 
 export default gameRouter;

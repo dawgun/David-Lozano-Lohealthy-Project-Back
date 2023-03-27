9bfc2b0da52da4f8b4e6e003e95031bb3f5c7aef
David <dawgun@hotmail.com>
2023-03-27 13:42:49 +0200

    Create gameOwnerChecker middleware

---
+++ src/server/middlewares/gameOwnerChecker/gameOwnerChecker.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Response } from "express";
+import Game from "../../../database/models/Game";
+import CustomRequest from "../../../types/customRequest";
+import CustomError from "../../../utils/CustomError/CustomError";
+
+const gameOwnerChecker = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const gameRequestId = req.body.id;
+  const ownerId = req.payload.id;
+
+  const game = await Game.findById(gameRequestId);
+
+  if (game.owner.toString() === ownerId) {
+    next();
+    return;
+  }
+
+  const customError = new CustomError(
+    400,
+    "Payload id and id game owner don't match",
+    "You do not own"
+  );
+
+  next(customError);
+};
+
+export default gameOwnerChecker;
